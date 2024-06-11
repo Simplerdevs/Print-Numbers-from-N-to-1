a51c0ae8570d85391ae070087a4b930025aa50bd
Simplerdevs <tplg7839@gmail.com>
2024-06-11 10:46:32 -0700

    Add project files.

---
+++ Print Numbers from N to 1/Print Numbers from N to 1.cpp
@@ -0,0 +1,28 @@
+
+/* Program to Print number from 1 to N */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+
+{
+
+    int num, counter = 10;
+    cout << "Please Enter a number ! " << endl;
+    cin >> num;
+
+    // we use For loops
+
+    for (counter = num; counter >= 1; counter--) {
+
+        cout << counter << endl;
+    }
+
+    return 0;
+
+    // Created by @ilyes_Trabelsi
+}
+
+
